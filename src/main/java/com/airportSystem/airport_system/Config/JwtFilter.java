a9a8248b1dfd5f26f608cd9a1721ca67b99cd185
kb-patel2005 <krishpatel1181@gmail.com>
2026-05-27 15:10:12 +0530

    cors 403 fix...

--- src/main/java/com/airportSystem/airport_system/Config/JwtFilter.java
+++ src/main/java/com/airportSystem/airport_system/Config/JwtFilter.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,32 +23,44 @@ public class JwtFilter extends org.springframework.web.filter.OncePerRequestFilt
     private PassengerDetailService PassengerDetailService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response, FilterChain filterChain)
-            throws java.io.IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response,FilterChain filterChain) throws IOException,ServletException, java.io.IOException {
+
+        // IMPORTANT FOR CORS
+        if (request.getMethod().equals("OPTIONS")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
         final String authorizationHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+        if (authorizationHeader != null &&authorizationHeader.startsWith("Bearer ")) {
+
             token = authorizationHeader.substring(7);
-            username = jwtUtils.extractUsername(token);
+
+            try {
+                username = jwtUtils.extractUsername(token);
+                System.out.println("USERNAME: " + username);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = PassengerDetailService.loadUserByUsername(username);
 
-            if (jwtUtils.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+            UserDetails userDetails =PassengerDetailService.loadUserByUsername(username);
 
-                authToken
-                        .setDetails(new org.springframework.security.web.authentication.WebAuthenticationDetailsSource()
-                                .buildDetails(request));
+            if (jwtUtils.validateToken(token,userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        
-        filterChain.doFilter(request, response);
+
+        filterChain.doFilter(request,response);
     }
 
 }
